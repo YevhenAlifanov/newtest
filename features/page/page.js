a71879f69afcd3f2dffba3026ed859be0fd7302e
Ivan <My email alifanovqa85@gmail.com>
2023-01-27 14:41:58 +0200

    maximize window

--- features/page/page.js
+++ features/page/page.js
@@ -1,38 +1,45 @@
 const assert = require('assert');
-const {Given, When, Then, After} = require('@cucumber/cucumber');
+// const {Given, When, Then, After} = require('@cucumber/cucumber');
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const driver = new Builder().forBrowser('chrome').build();
 
 class Basepage{
     constructor(){
         this.driver = driver;
-
     }
+
     async maximizeWindow() {
         await this.driver.manage().window().maximize()
     }
+
     async visit(url) {
         await this.driver.get(url)
     }
+
     async sendRequestLinkClick(){
         await this.click(locators.sendRequestButton, 5000)
     }
+
     async find(locator, timeout) {
         await this.driver.wait(until.elementLocated(By.xpath(locator)), timeout); //TODO check is it really need
         return this.driver.findElement(By.xpath(locator));
     }
+
     async click(locator, timeout) {
         await this.driver.wait(until.elementLocated(By.xpath(locator)), timeout);
         await this.driver.findElement(By.xpath(locator)).click()
     }
+
     async type(locator, timeout, inputText) {
         await this.driver.wait(until.elementLocated(By.xpath(locator)), timeout);
         await this.driver.findElement(By.xpath(locator)).sendKeys(inputText, Key.TAB)
     }
+
     async getText(locator, timeout) {
         let el = await this.driver.wait(until.elementLocated(By.xpath(locator)), timeout)
         return await el.getText();
     }
+
     async clickOnTheElement(xpath, Timeout) {
         await driver.wait(until.elementLocated(By.xpath(xpath)), Timeout).click();
     };
@@ -40,4 +47,5 @@ class Basepage{
 
 }
 
-module.exports = {Basepage, driver}
+module.exports = Basepage;
+module.exports = driver;
--- features/step_definitions/MySteps.js
+++ features/step_definitions/MySteps.js
@@ -1,14 +1,13 @@
-// import {Given} from 'cucumber';
 require('chromedriver');
 const assert = require('assert');
 const {Given, When, Then, After} = require('@cucumber/cucumber');
 const locator = require('../locators/locator');
-const page = require('../page/page');
+const page = require('../page/page.js');
 const timeout = 5000;
 
 Given(/^Maximize browser window$/, async function () {
  // await page.maximizeWindow()
-    await this.driver.manage().window().maximize()
+    await page.driver.manage().window().maximize()
 });
 
 Given(/^User goes to AimChat$/, async function () {
